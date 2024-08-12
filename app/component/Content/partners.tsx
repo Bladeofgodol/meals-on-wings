34549cace17fcfc032ae84db4039821e6a560788
dan <dan@arifpay.org>
2024-08-12 11:51:00 +0300

    marquee auto fill

--- app/component/Content/partners.tsx
+++ app/component/Content/partners.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import Marquee from "react-fast-marquee";
-import "/public/styles/marquee.css";
+import "../../../public/styles/marquee.css";
 const Partners = () => {
   return (
     <div className="w-screen flex justify-center p-20 flex-col  items-center">
       <div className="text-4xl md:text-6xl font-bold text-gray-700">
         PARTNERS
       </div>
-      <div className="w-fit flex p-40">
-        <Marquee className="marquee">
+      <div className="w-full flex p-40">
+        <Marquee className="marquee" autoFill>
           <div>hi</div>
           <div>bye</div>
         </Marquee>
